2eb625b24f88b56dc293f036de7aa530c66fcb2e
Matt Galvin <matthewgalvin908@gmail.com>
2013-04-01 17:41:05 -0400

    merged, changed johns number to mine

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ u2 = User.create(:name => 'stevehotel', :email => 'stevehotel@gmail.com', :passw
 u3 = User.create(:name => 'matthotel', :email => 'matthotel@gmail.com', :password => 'x', :password_confirmation => 'x')
 
 r1 = Reservation.create(name: 'stephen', phone: '5164747688', date: '2013-03-22', day: 'fri', party_size: 2, est_wait: 20, act_wait: 23)
-r2 = Reservation.create(name: 'john', phone: '5164747688', date: '2013-03-22', day: 'fri', party_size: 4, est_wait: 15, act_wait: 13)
+r2 = Reservation.create(name: 'john', phone: '9086421700', date: '2013-03-22', day: 'fri', party_size: 4, est_wait: 15, act_wait: 13)
 r3 = Reservation.create(name: 'mack', phone: '5164747688', date: '2013-03-22', day: 'fri', party_size: 5, est_wait: 10, act_wait: 10)
 r4 = Reservation.create(name: 'jill', phone: '5164747688', date: '2013-03-22', day: 'fri', party_size: 2, est_wait: 30, act_wait: 35)
 r5 = Reservation.create(name: 'liz', phone: '5164747688', date: '2013-03-23', day: 'sat', party_size: 4, est_wait: 40, act_wait: 38)
