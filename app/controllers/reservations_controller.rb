09fd051fcf6d4b69b3b63feaafa894bb21835070
Matt Galvin <matthewgalvin908@gmail.com>
2013-04-14 17:44:55 -0400

    pulling

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -19,14 +19,11 @@ class ReservationsController < ApplicationController
   end
 
   def sendtext
-    binding.pry
     name = params[:first]
     body = params[:body]
     reservation = Reservation.where(:name => name).first
-    # reservation.is_texted: = true
     client = Twilio::REST::Client.new(ENV['TW_SID'], ENV['TW_TOK'])
     client.account.sms.messages.create(:from => '17324126143', :to => reservation.phone, :body => body)
-    # render :js => 'sendtext', :test => 'heyo'
   end
 
 end
